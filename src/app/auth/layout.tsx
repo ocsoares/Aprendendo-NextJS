914c287afc7e0033e0f05d6526ccd52549d1d827
ocsoares <casoares016@gmail.com>
2023-09-30 17:07:47 -0300

    feat: add Header with nextjs metadata

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,6 +1,13 @@
 // Layout APENAS para as Rotas de Auth !!!
 
+import { Metadata } from "next";
 import { PropsWithChildren } from "react";
+
+// Header APENAS para minhas Rotas de AUTH !!!
+export const metadata: Metadata = {
+  title: "AUTH Header",
+  description: "Header de minhas Rotas de Auth !",
+};
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 // Esse Layout é Aplicado em TODAS as ROTAS !!!
 
 import { PropsWithChildren } from "react";
+import { Metadata } from "next";
+
+// Header do HTML !!!
+// ATENÇÃO: Colocar o NOME EXATAMENTE como "metadata" !!!!
+// ---------------------------
+// Pelo o que eu PERCEBI, PODE usar isso em QUALQUER Página, mas
+// acho que é melhor usar em Layout !!!
+export const metadata: Metadata = {
+  title: "Header INICIAL",
+  description: "Esse é apenas o Header inicial do meu projeto NextJS",
+  // mais configs...
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
