b336ac028b2c3e01ed61053cd8b909b00f3f5eff
ocsoares <casoares016@gmail.com>
2023-09-30 17:50:28 -0300

    feat: add route file

---
+++ src/app/route.tsx
@@ -0,0 +1,24 @@
+// TIRAR DEPOIS !!
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// EXEMPLO do Próprio NextJS !!!
+
+export async function GET(request: Request) {}
+
+// Context (OPTIONAL) !!!
+// export async function GET(request, context: { params }) {
+//   const { team } = params; // '1'
+// }
+
+export async function HEAD(request: Request) {}
+
+export async function POST(request: Request) {}
+
+export async function PUT(request: Request) {}
+
+export async function DELETE(request: Request) {}
+
+export async function PATCH(request: Request) {}
+
+// If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and  set the appropriate Response `Allow` header depending on the other methods defined in the route handler.
+export async function OPTIONS(request: Request) {}
